cafae79d5cf894a644c6c2c1549357a4fe19918b
CH4A0 <jzhajao2305@g.educaand.es>
2025-04-11 22:28:06 +0200

    Arreglado errores de plantilla y links

--- actividad1/src/app/app.component.ts
+++ actividad1/src/app/app.component.ts
@@ -1,19 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true, // Indica que el componente es standalone
+  template: `
+  <form [formGroup]="profileForm" (ngSubmit)="onSubmit()">
+    <input type="text" formControlName="name" name="name" placeholder="Nombre" />
+    <input type="email" formControlName="email" name="email" placeholder="Correo electrónico" />
+    <button type="submit" [disabled]="!profileForm.valid">Enviar</button>
+  </form>
+  `,
+  styleUrls: ['./app.component.scss'], // Se mantiene la referencia a los estilos
+  imports: [CommonModule, ReactiveFormsModule], // Importamos los módulos necesarios
 })
 export class AppComponent {
   title = 'actividad1';
-  // creamos el formulario reactivo
+  
   profileForm = new FormGroup({
-    // ponemos los controles del formulario
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    name: new FormControl('', Validators.required), // Campo obligatorio
+    email: new FormControl('', [Validators.required, Validators.email]), // Campo obligatorio con email
   });
+
+  // Método para manejar el envío del formulario
+  onSubmit() {
+    if (this.profileForm.valid) {
+      console.log('Formulario enviado:', this.profileForm.value);
+      alert('¡Formulario enviado correctamente!');
+    }
+  }
 }
