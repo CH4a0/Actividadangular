8bbec34e75a232de7956afb38748b99eb1a0092f
CH4A0 <jzhajao2305@g.educaand.es>
2025-04-11 22:03:49 +0200

    Creamos la validacion de formulario

--- actividad1/src/app/app.component.ts
+++ actividad1/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'actividad1';
+  // creamos el formulario reactivo
+  profileForm = new FormGroup({
+    // ponemos los controles del formulario
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
 }
